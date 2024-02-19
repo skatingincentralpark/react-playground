5a6b350602c87d4231f42b5142e3fb9101f52d97
Charlie Zhao <charles.zhao5461@gmail.com>
2024-02-19 13:57:42 +1100

    Use greatfrontend solution, remove usecallback and useeffect usage

--- app/exercises/progress-bar/page.tsx
+++ app/exercises/progress-bar/page.tsx
@@ -1,43 +1,9 @@
 "use client";
 
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
 import "./styles.css";
 
-function ProgressBar({
-  onCompleted,
-  ready,
-}: {
-  onCompleted: () => void;
-  ready: boolean;
-}) {
-  const [progress, setProgress] = useState(0);
-
-  useEffect(() => {
-    if (!ready) return;
-
-    const interval = setInterval(() => {
-      setProgress((p) => {
-        if (p >= 100) {
-          clearInterval(interval);
-
-          return 100;
-        }
-
-        return p + 0.005 * 100;
-      });
-    }, 10);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [ready, onCompleted]);
-
-  useEffect(() => {
-    if (progress >= 100) {
-      onCompleted();
-    }
-  }, [progress, onCompleted]);
-
+function ProgressBar({ progress }: { progress: number }) {
   return (
     <div className="progess-bar" role="progressbar">
       <div
@@ -49,42 +15,63 @@ function ProgressBar({
   );
 }
 
-const CONCURRENCY_COUNT = 3;
+const CONCURRENCY_LIMIT = 3;
 
 export default function Page() {
-  const [bars, setBars] = useState(0);
-  const [completed, setCompleted] = useState(0);
-  const [paused, setPaused] = useState(false);
+  const [progression, setProgression] = useState<number[]>([]);
+  const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
 
-  const onCompleted = useCallback(
-    () => setCompleted((c) => c + 1),
-    [setCompleted]
-  );
+  function start() {
+    const interval = setInterval(() => {
+      setProgression((currProgression) => {
+        const nonFull = currProgression
+          .map((progress, index) => ({ progress, index }))
+          .filter((b) => b.progress < 100);
+
+        if (nonFull.length === 0) {
+          return currProgression;
+        }
+
+        const toFill = nonFull.slice(0, CONCURRENCY_LIMIT);
+
+        const newProgression = currProgression.slice();
+        toFill.forEach(({ index }) => {
+          newProgression[index] = Math.min(100, newProgression[index] + 0.5);
+        });
+
+        return newProgression;
+      });
+    }, 10);
+
+    setTimer(interval);
+  }
+
+  function stop() {
+    if (timer) {
+      clearInterval(timer);
+      setTimer(null);
+    }
+  }
+
+  const isPaused = !timer;
+  const completed = progression.filter((b) => b === 100).length;
 
   return (
     <div className="flex flex-col gap-2">
       <div className="flex gap-2">
-        <button onClick={() => setBars((b) => b + 1)}>Add</button>
-        <button onClick={() => setPaused((p) => !p)}>
-          {paused ? "Resume" : "Pause"}
+        <button onClick={() => setProgression((b) => b.concat(0))}>Add</button>
+        <button onClick={isPaused ? start : stop}>
+          {isPaused ? "Resume" : "Pause"}
         </button>
         <div className="info-label">
-          Completed: {completed} / {bars}
+          Completed: {completed} / {progression.length}
         </div>
-        <div className="info-label">Max bars at once: {CONCURRENCY_COUNT}</div>
+        <div className="info-label">Max bars at once: {CONCURRENCY_LIMIT}</div>
       </div>
       <div className="flex flex-col gap-2">
-        {Array(bars)
-          .fill(null)
-          .map((_, i) => (
-            <ProgressBar
-              key={i}
-              onCompleted={onCompleted}
-              ready={
-                !paused && (i === 0 || i <= completed + CONCURRENCY_COUNT - 1)
-              }
-            />
-          ))}
+        {progression.map((progress, i) => (
+          <ProgressBar key={i} progress={progress} />
+        ))}
       </div>
     </div>
   );
